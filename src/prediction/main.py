2eff8a899bf5727a78d8b4625f7309c2d0ae6271
nielsaak <nielskrogsgaard96@gmail.com>
2025-05-21 09:48:57 +0200

    Added data

--- src/prediction/main.py
+++ src/prediction/main.py
@@ -7,8 +7,12 @@ if __name__ == "__main__":
     output_path_figures = os.path.join(current_dir, "output/figures")
     output_path_data = os.path.join(current_dir, "output/results")
 
+    # Load data
+    data_path = "data/results.processed (1).jsonl"
+    rank_path = "data/european_all_simplified.csv"
+
     # Model file path
-    model_file = os.path.join(current_dir, "../models/stan_model.stan")
+    model_file = os.path.join(current_dir, "src/models/final/stan_model.stan")
 
     # Create an instance of Prediction
     prediction = Prediction(model_file=model_file,
@@ -16,7 +20,7 @@ if __name__ == "__main__":
                             output_path_data=output_path_data)
 
     # Load data for prediction
-    prediction.load_data()
+    prediction.load_data(data_path=data_path)
 
     # Preprocess data for prediction
     prediction.preprocess_data()
--- src/prediction/utils/prediction.py
+++ src/prediction/utils/prediction.py
@@ -1,8 +1,10 @@
 
 
 class Prediction:
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, model_file, output_path_figures, output_path_data):
+        self.model_file = model_file
+        self.output_path_figures = output_path_figures
+        self.output_path_data = output_path_data
 
     def load_data(self, data_path):
         # Load your data here
